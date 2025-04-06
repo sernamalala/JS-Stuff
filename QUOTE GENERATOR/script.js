3f9e62e4c9ba2f79f534c5aca1ced3cea46317ab
sernamalala <sm.sernamalala@gmail.com>
2025-04-06 13:33:05 +0200

    finalised the new quote and tweet button

--- QUOTE GENERATOR/script.js
+++ QUOTE GENERATOR/script.js
@@ -1,29 +1,34 @@
 let responseReceived;
 
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/quotes',
-    headers: { 'X-Api-Key': '0CGx7blJkimNd1hJWmC+Tg==QKt2c8438I37pUC0'},
-    contentType: 'application/json',
-    success: function(result) {
-       console.log(result)
-        displayQuote(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
+function getQuote() {
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/quotes',
+        headers: { 'X-Api-Key': '0CGx7blJkimNd1hJWmC+Tg==QKt2c8438I37pUC0'},
+        contentType: 'application/json',
+        success: function(result) {
+           console.log(result)
+            displayQuote(result);
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    });
+}
 
+getQuote();
 const quoteDiv = document.getElementById("quote");
-
+let quoteText; 
+let author;
 function displayQuote(arr){
 
+    quoteDiv.innerHTML = "";
     let object = arr[0]
-    let quote = object.quote
-    let author = object.author
+    quoteText = object.quote
+    author = object.author
 
     let p1 = document.createElement("p");
-    p1.textContent = quote;
+    p1.textContent = quoteText;
     p1.id = "p1";
     let p2 = document.createElement("p");
     p2.textContent = author;
@@ -36,6 +41,12 @@ function displayQuote(arr){
 const tweet = document.getElementById("tweet");
 const newQuote = document.getElementById("new-quote");
 
+
 tweet.addEventListener("click", function () {
-    
+    const tweetUrl = `https://x.com/intent/tweet?text=${encodeURIComponent(quoteText)} - ${author}`;
+    window.open(tweetUrl, "_blank");
+})
+
+newQuote.addEventListener("click", function () {
+    getQuote();
 })
